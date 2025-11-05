027c087f081aef86ddd6d776391d9438186fc255
Fernando Camargo <fcamargoa@miumg.edu.gt>
2025-11-05 23:22:39 +0000

    Prueba a hostinger

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,16 +6,21 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // No necesitas registrar HandleCors aquí, Laravel lo aplica automáticamente
+        // desde config/cors.php
+
+        // Alias de middlewares personalizados
         $middleware->alias([
             'jwt' => \App\Http\Middleware\JwtMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
