2859471f0552f15ad06e05e25c403ebb3a31ec9d
Fernando Camargo <fcamargoa@miumg.edu.gt>
2025-11-06 20:21:33 -0600

    prueba para laravel api

--- config/cors.php
+++ config/cors.php
@@ -5,15 +5,29 @@ return [
     
     'allowed_methods' => ['*'],
     
-    'allowed_origins' => ['*'],  // Temporal para pruebas
+    'allowed_origins' => [
+        // Desarrollo local
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:5173',
+        
+        // Producción (usar variable de entorno)
+        ...array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', ''))),
+    ],
     
     'allowed_origins_patterns' => [],
     
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [
+        'Accept',
+        'Content-Type', 
+        'Authorization',
+        'X-Requested-With',
+    ],
     
     'exposed_headers' => [],
     
-    'max_age' => 0,
+    'max_age' => 3600, // Cache preflight por 1 hora
     
-    'supports_credentials' => false,  // Cambiar a false temporalmente
+    'supports_credentials' => true,
 ];
